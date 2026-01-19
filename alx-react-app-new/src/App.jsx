b4bd796279e1b67ae2202fa91bc3487d4cc738a0
Serwaa19 <motifrempong@gmail.com>
2026-01-19 12:30:46 +0000

    fixed

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import UserProfile from "../../alx-react-app-new/alx-react-app/src/components/UserProfile";
-import Header from '../../alx-react-app-new/alx-react-app/src/components/Header';
-import MainContent from '../../alx-react-app-new/alx-react-app/src/components/MainContent';
-import Footer from '../../alx-react-app-new/alx-react-app/src/components/Footer';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import UserProfile from "../../alx-react-app-new/src/components/UserProfile";
+import Header from '../../alx-react-app-new/src/components/Header';
+import MainContent from '../../alx-react-app-new/src/components/MainContent';
+import Footer from '../../alx-react-app-new/src/components/Footer';
+import { useState } from 'react';
 import './App.css'
 
 function App() {
@@ -36,14 +34,6 @@ function App() {
       <MainContent />
       <Footer />
     </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
