daf698c283de738f9fc08a6056797aa5bce105ec
Serwaa19 <motifrempong@gmail.com>
2026-01-19 11:40:58 +0000

    Counter Application using States and Hooks

--- alx-react-app-new/alx-react-app/src/App.jsx
+++ alx-react-app-new/alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         bio="An IT student who loves building React applications."
       />
       <Footer />
+      <Counter />
     </div>
   );
 }
