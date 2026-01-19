39d818235f6056fa19d835746b5a7e3f08845552
Serwaa19 <motifrempong@gmail.com>
2026-01-19 21:28:01 +0000

    userprofile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import UserContext from './components/UserContext';
+import UserContext from './UserContext';
 import UserDetails from './components/UserDetails';
 
 function UserProfile() {
