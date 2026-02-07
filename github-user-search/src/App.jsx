de97d86f280159e7787c958a725fe6cff4f73b9a
Serwaa19 <motifrempong@gmail.com>
2026-02-07 07:38:10 +0000

    Integrate GitHub API and add user search feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,10 @@
-import Header from "./components/Header";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>GitHub User Search</h1>
-      <p>Search for GitHub users using the GitHub API</p>
+      <Search />
     </div>
   );
 }
